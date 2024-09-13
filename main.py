b05f88026a5bca0617ddba8924a974577e6e3921
Ali Farooq <alifarooq.zenkoders@gmail.com>
2024-09-13 12:59:03 -0700

    implemented langchain terminal bot

---
+++ main.py
@@ -0,0 +1,41 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+
+memory = InMemoryChatMessageHistory()
+
+
+def get_history():
+    return memory
+
+
+prompt = ChatPromptTemplate(messages=[
+    MessagesPlaceholder(variable_name='messages'),
+    ('human', '{content}'),
+]
+)
+
+lcel_chain = prompt | model | StrOutputParser()
+
+chain = RunnableWithMessageHistory(
+    lcel_chain,
+    get_history,
+    input_messages_key="content",
+    history_messages_key="messages"
+)
+
+
+while True:
+    content = input(">> ")
+    result = chain.invoke(
+        {'content': content})
+
+    print(result)
